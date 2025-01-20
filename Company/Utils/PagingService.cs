50feb1e17055ec46c8c2ac280c47aaa8ad04d395
imaravelias <giannismaravelias@yahoo.com>
2025-01-20 17:59:21 +0200

    Added paging Service
    Improve paging Service

---
+++ Company/Utils/PagingService.cs
@@ -0,0 +1,34 @@
+﻿using CompanyWork.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace CompanyWork.Utils
+{
+    public class PagingService<T>(MyDbContext db)
+    {
+        private readonly MyDbContext _db = db;
+        public async Task<List<T>> PageData(IQueryable<T> data, int? pageNumber, int? pageSize)
+        {
+            List<T> listToPage = new();
+
+            if (!pageNumber.HasValue)
+                return null;
+
+            if (!pageSize.HasValue)
+                return null;
+
+
+            listToPage = await data
+                .OrderBy(d => data) //check this again
+                .Skip(pageNumber.Value * pageSize.Value) //0, 5, 10, 15 ... 
+                .Take(pageSize.Value)
+                .ToListAsync();
+
+
+
+            return listToPage;
+
+        }
+    }
+}
