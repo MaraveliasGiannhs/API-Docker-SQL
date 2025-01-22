01b1ca254085e80fc93b1049f7bcac7d00e1cff4
imaravelias <giannismaravelias@yahoo.com>
2025-01-22 18:01:29 +0200

    .

---
+++ Company/Interfaces/IPostUpdate.cs
@@ -0,0 +1,13 @@
+﻿using CompanyWork.Models;
+using CompanyWork.PersistClasses;
+using static CompanyWork.Models.AssetDTO;
+
+namespace CompanyWork.Interfaces
+{
+    public interface IPostUpdate<T,C>
+    {
+        Task<List<T>> PostUpdateAsync(C persistDTO);
+
+    }
+
+}
