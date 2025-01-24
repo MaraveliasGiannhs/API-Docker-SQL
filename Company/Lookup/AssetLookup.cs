a9d0df26c349c82ba8fa741706c25937429ef638
imaravelias <giannismaravelias@yahoo.com>
2025-01-24 17:32:07 +0200

    Added required fields for paging Added required fields for sorting

--- Company/Lookup/AssetLookup.cs
+++ Company/Lookup/AssetLookup.cs
@@ -4,8 +4,11 @@
     {
         public Guid? Id { get; set; }
         public string Like { get; set; }
-        public int? SelectedPage { get; set; }
-        
+        public int? PageIndex { get; set; }
+        public int? ItemsPerPage { get; set; }
+        public string? OrderItem { get; set; }
+        public bool? AscendingOrder { get; set; }
+
         //add paging and ordering ?
 
     }
