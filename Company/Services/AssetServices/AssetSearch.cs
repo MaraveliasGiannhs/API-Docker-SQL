0e9bb0eb432ff023fe4c1fa92f39a1c553bcbad3
imaravelias <giannismaravelias@yahoo.com>
2025-01-24 17:34:08 +0200

    Added chaining methods, null checking Added sorting & paging method

--- Company/Services/AssetServices/AssetSearch.cs
+++ Company/Services/AssetServices/AssetSearch.cs
@@ -1,10 +1,8 @@
 ﻿using CompanyWork.Data;
-using System.Linq;
-using CompanyWork.Interfaces;
 using CompanyWork.Models;
 using CompanyWork.Lookup;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace CompanyWork.Services.AssetServices
 {
@@ -12,53 +10,120 @@ namespace CompanyWork.Services.AssetServices
     {
         private readonly MyDbContext _db = db;
 
+        private Guid? _id;
+        private string? _name;
+        private int? _pageIndex;
+        private int? _itemsPerPage;
+        private string? _orderByItem;
+        private bool _ascending;
+
+        public AssetSearch Ids(Guid id)
+        {
+            _id = id;
+            return this;//for chaining
+        }
+
+        public AssetSearch Names(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public AssetSearch PageIndex(int pageIndex)
+        {
+            _pageIndex = pageIndex;
+            return this;
+        }
+
+        public AssetSearch PageSize(int itemsPerPage)
+        {
+            _itemsPerPage = itemsPerPage;
+            return this;
+        }
+        public AssetSearch OrderBy(string? orderByItem)
+        {
+            _orderByItem = orderByItem;
+            return this;
+        }
+
+        public AssetSearch Ascending(bool ascending)
+        {
+            _ascending = ascending;
+            return this;
+        }
+
         public async Task<List<AssetDTO>> SearchTermAsync(AssetLookup lookup)
         {
             if (lookup == null)
                 throw new NotImplementedException("Lookup term is null");
 
-
             IQueryable<Asset> assetDb = _db.Asset; // all items
 
-
             // Filters
-            if (!string.IsNullOrWhiteSpace(lookup.Like))
-                assetDb = assetDb.Where(a => a.Name.Contains(lookup.Like.ToLower()));
+            if (!string.IsNullOrWhiteSpace(this._name))
+                assetDb = assetDb.Where(a => a.Name.Contains(this._name.ToLower()));
 
-            if (lookup.Id.HasValue)
-                assetDb = assetDb.Where(a => a.Id == lookup.Id.Value);
+            if (this._id.HasValue)
+                assetDb = assetDb.Where(a => a.Id == this._id);
 
-            List<Asset> assetList = await PageData(assetDb, 1, 3);
-            //List<Asset> assetList = await assetDb.ToListAsync();
+            if (!string.IsNullOrEmpty(this._orderByItem))
+                assetDb = OrderByDynamically(assetDb, this._orderByItem, this._ascending);
+
+            Console.WriteLine(_orderByItem);
+            Console.WriteLine(_ascending);
+
+
+            List<Asset> assetList = await PageData(assetDb, _pageIndex, _itemsPerPage);
             List<AssetDTO> assetDTO = await AssetDTO.MapAssets(_db, assetList);
 
             return assetDTO;
         }
 
 
+        public async Task<int> Count()
+        {
+            IQueryable<Asset> assetDb = _db.Asset;
 
+            //foreach (var item in listToPage) //slightly faster than linq Count
+            //    allItems++;
+            //Console.WriteLine("All items:" + allItems);
+            return await assetDb.CountAsync();
+        }
+
+
+        public IQueryable<Asset> OrderByDynamically(IQueryable<Asset> data, string orderByItem, bool ascending)
+        {
+            var parameter = Expression.Parameter(typeof(Asset), "a"); 
+            var property = Expression.Property(parameter, orderByItem); 
+
+            var lambda = Expression.Lambda<Func<Asset, object>>(
+                Expression.Convert(property, typeof(object)),//body 
+                parameter); //input (a)
+
+
+            if (!ascending)
+                return data.OrderByDescending(lambda);
+            else
+                return data.OrderBy(lambda);
+        }
 
         public async Task<List<Asset>> PageData(IQueryable<Asset> data, int? selectedPage, int? pageSize)
         {
             List<Asset> listToPage = new();
 
-            //if (!pageNumber.HasValue)
-            //    return null;
+            if (!selectedPage.HasValue)
+                throw new ApplicationException();
 
             if (!pageSize.HasValue)
-                return null;
+                throw new ApplicationException();
 
 
             listToPage = await data
-                .OrderBy(d => d.Name) //check this again
-                .Skip(selectedPage.Value * pageSize.Value) //0, 5, 10, 15 ... 
+                .Skip((selectedPage.Value - 1) * pageSize.Value) //0, 5, 10, 15 ... 
                 .Take(pageSize.Value)
                 .ToListAsync();
 
-
-
             return listToPage;
-
         }
     }
 }
